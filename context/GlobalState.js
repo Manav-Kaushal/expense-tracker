88cffdedc324e410a5f7ec37ca9ad6f7bc7769f8
Manav Kaushal <manavkaushal756@gmail.com>
2022-03-20 08:49:46 +0530

    backend complete

--- context/GlobalState.js
+++ context/GlobalState.js
@@ -1,14 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20, date: "2022-03-19T22:11:01.214Z" },
-    { id: 2, text: "Salary", amount: 300, date: "2022-03-19T22:11:01.214Z" },
-    { id: 3, text: "Book", amount: -10, date: "2022-03-19T22:11:01.214Z" },
-    { id: 4, text: "Camera", amount: 150, date: "2022-03-19T22:11:01.214Z" },
-  ],
+  transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Create context
@@ -19,11 +17,58 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
-  function deleteTransaction(id) {
-    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  async function getTransactions() {
+    try {
+      const res = await axios.get("http://localhost:5000/api/v1/transactions", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.error,
+      });
+    }
   }
-  function addTransaction(transaction) {
-    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+
+  async function deleteTransaction(id) {
+    try {
+      await axios.delete(`http://localhost:5000/api/v1/transactions/${id}`);
+
+      dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.error,
+      });
+    }
+  }
+
+  async function addTransaction(transaction) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/transactions/",
+        transaction,
+        config
+      );
+      dispatch({ type: "ADD_TRANSACTION", payload: res.data.data });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.error,
+      });
+    }
   }
 
   return (
@@ -32,6 +77,9 @@ export const GlobalProvider = ({ children }) => {
         transactions: state.transactions,
         deleteTransaction,
         addTransaction,
+        getTransactions,
+        error: state.error,
+        loading: state.loading,
       }}
     >
       {children}
